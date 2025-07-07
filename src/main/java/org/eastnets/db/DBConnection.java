5fd654896a41ea35f0cd834243f4d012c0cf1fde
Hazem Abdeen <hazem.abdeen39@gmail.com>
2025-07-07 10:41:28 +0300

    Implement file-based DB config and HikariCP pooling

--- src/main/java/org/eastnets/db/DBConnection.java
+++ src/main/java/org/eastnets/db/DBConnection.java
@@ -1,14 +1,36 @@
 package org.eastnets.db;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.io.InputStream;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/library_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "225588";
+
+    private static final HikariDataSource dataSource;
+
+    static {
+        Properties props = new Properties();
+        try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new RuntimeException("db.properties not found");
+            }
+            props.load(input);
+        } catch (IOException ex) {
+            throw new RuntimeException("Error loading database properties", ex);
+        }
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(props.getProperty("db.url"));
+        config.setUsername(props.getProperty("db.user"));
+        config.setPassword(props.getProperty("db.password"));
+        config.setMaximumPoolSize(10);
+        dataSource = new HikariDataSource(config);
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return dataSource.getConnection();
     }
 }
